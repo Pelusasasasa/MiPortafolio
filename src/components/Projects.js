6f43d5ce2d2a6171d58f72727d69529b2818bc93
Pelusa <agustinlorenzatto@hotmail.com>
2022-02-24 10:53:36 -0300

    Agregue projectos hechos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,15 +13,48 @@ const Projects = () => {
             <img src="img/AppCripto.png" alt="" />
           </div>
           <div className="lado atras">
-              <h2>Titulo</h2>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laborum fugit, ipsa iusto ipsam earum natus,
-                 repudiandae unde ad sint aperiam nisi odit fugiat tempore. Iusto perferendis eaque eos totam.
+              <h2>Cripto App</h2>
+              <p>Es un projecto sencillo hecho en react que muestra las primeras 100 criptomonedas en el ranking y muestra informacion sobre ellas donde al tocar sobre un renglon salamos a otra donde nos da mas informacion sobre la misma.
               </p>
-              <button>Page</button>
-              <button>Github</button>
+              <div className="button">
+                <button><a href="#">Page</a></button>
+                <button><a target="_blank" href="https://github.com/Pelusasasasa/MiPortafolio">Github</a></button>
+              </div>
           </div>
         </div>
       </div>
+
+        <div className="contenedor">
+          <div className="carta">
+            <div className="lado frente">
+              <img src="img/sistemaFacturacion.png" alt="" />
+            </div>
+            <div className="lado atras">
+                <h2>Sistema de Facturacion</h2>
+                <p>Se trata de una aplicacion hecha en electron.js para un sistema de facturacion,control de stock y distintos funcionamientos para comercio donde se conecta a un servidor en la red interna.
+                </p>
+               <div className="button">
+                <button><a target="_blank" href="https://github.com/Pelusasasasa/sistemaFacturacion">Github</a></button>
+               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="contenedor">
+          <div className="carta">
+            <div className="lado frente">
+              <img src="img/Servidor.png" alt="" />
+            </div>
+            <div className="lado atras">
+                <h2>Servidor</h2>
+                <p>Servidor usando express,node.js y mongodb con librerias como mongoose para trabajar datos de un sistema de facturacion de un comercio con su propia api rest.
+                </p>
+                <div className="button">
+                  <button><a target="_blank" href="https://github.com/Pelusasasasa/servidor">Github</a></button>
+                </div>
+            </div>
+          </div>
+        </div>
     </section>
     </main>
   )
